a188691aac11ea6d7dcd1e5d3d34a01b94b93401
Shidil Dinesh <shidil@live.com>
2015-05-31 16:59:44 +0530

    make more responsive, show menu,sidebar toggles working

--- js/site.js
+++ js/site.js
@@ -2,3 +2,33 @@
 // defined in coreJS.js
 var cJS = new coreJS();
 
+
+function focusSearch(node){
+	node.parentNode.classList.add("focused");
+	node.parentNode.children[1].classList.remove("hide");
+	node.parentNode.children[2].classList.remove("hide");
+}
+function blurSearch(node){
+	node.parentNode.classList.remove("focused");
+	node.parentNode.children[0].value = "";
+	node.parentNode.children[1].classList.add("hide");
+	node.parentNode.children[2].classList.add("hide");
+}
+function showHideSidebarToggle(){
+	var menu = document.getElementById("board-menu");
+	if(menu.className === "show"){
+		menu.className = "";
+	}
+	else{
+		menu.className = "show";
+	}
+}
+function showHideSidebarMenuToggle(){
+	var menu = document.querySelectorAll("#board-menu ul")[0];
+	if(menu.className === "show"){
+		menu.className = "";
+	}
+	else{
+		menu.className = "show";
+	}
+}
